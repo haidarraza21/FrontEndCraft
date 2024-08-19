e3a6e6806efa96018f32841c33ac23ab5fdf57a9
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-08-19 14:10:20 +0000

    Create JavaScript filder

---
+++ JavaScript/Notes/arithmetic.js
@@ -0,0 +1,41 @@
+/*Arithmetic operators:
+Arithmetic operators in JavaScript are used to perform mathematical operations on numbers. These operators allow you to add, subtract, multiply, divide, and find the remainder of values. Here's a list of the basic arithmetic operators: */
+
+/**Addition (+)
+Adds two numbers together. */
+
+let a = 45;
+let b =5;
+
+let add = a + b;
+console.log(add);
+
+/**Subtraction (-)
+Subtracts the second number from the first. */
+
+let subtract = a - b;
+console.log(subtract);
+
+/**Multiplication (*)
+Multiplies two numbers. */
+
+let multiply = a * b;
+console.log(multiply);
+
+/**Division (/)
+Divides the first number by the second.*/
+
+let division = a / b;
+console.log(division);
+
+/**Modulus (%)
+Returns the remainder of the division of the first number by the second. */
+
+let Modulus = a % b;
+console.log(Modulus);
+
+/** Exponentiation (**)
+Raises the first number to the power of the second.*/
+
+let Exponentiation = a ** b;
+console.log(Exponentiation);
