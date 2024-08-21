662b5dd759d3f6b45611bd72a459e03c39ec1703
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-08-21 14:21:46 +0000

    Add

---
+++ JavaScript/PracticeSet/Level2/variables.js
@@ -0,0 +1,12 @@
+/*
+1.Save your name in a variable.
+2. Display name from the variable 
+3. Calculate the cost of Myntra Bag and keep it in a variable
+4. Show it to console
+5. Keep GST parcentage as constant
+6. Use eval method from math to convart sring calculator into result
+ */
+
+let myName = "Md Haidar Raza";
+console.log(myName);
+
