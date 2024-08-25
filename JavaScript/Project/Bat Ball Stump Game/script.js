a969907f8c84db1907a0a031af531adbf442b3d6
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-08-25 11:31:16 +0000

    update function

--- JavaScript/Project/Bat Ball Stump Game/script.js
+++ JavaScript/Project/Bat Ball Stump Game/script.js
@@ -8,7 +8,39 @@ function generateComputerChoice(){
         return 'Ball';
     } else{
         return 'Stump';
-    }
+    }    
+}
 
-    
+function getResult(userMove, computerMove){
+    if(userMove === 'Bat'){
+        if(computerMove === 'Ball'){
+           return 'User won!';
+        } else if(computerMove === 'Bat'){
+           return `It's a tie!`
+        } else{
+          return ` Computer has won!`;
+        }
+    } else if (userMove == 'Bat') {
+        if(computerMove === 'Ball'){
+            return `It's a tie!`;
+        } else if (computerMove === 'Bat'){
+            return `Computer has won!`;
+        } else {
+            return `User won!`;
+        }
+
+    } else {
+        if(computerMove === 'Ball'){
+            return `User won!`;
+        } else if(computerMove === 'Bat'){
+            return `Computer has won!`;
+        } else{
+            return `It's a tie!`;
+        }
+    }
+}
+
+function showResult(userMove, computerMove, result){
+    alert(`You have choose ${userMove}. Computer choice is ${computerMove} and ${result}`)
+
 }
